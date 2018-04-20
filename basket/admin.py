29211571cd68967282f8b0b3c9b01125a7c14d89
matiastejos <m.tejosbucarey@uandresbello.edu>
2018-04-20 20:36:25 -0300

    avance imagen

--- basket/admin.py
+++ basket/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from basket.models import *
 
 # Register your models here.
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display=('name','thumb' )
+    search_fields=('name',)
+    def thumb(self,obj):
+        return mark_safe("<img src='%s' width='40' height='40'>"% obj.logo.url)
+    thumb.allow_tags = True
+    thumb.__name__ = 'logo'
+
+#admin.site.register(Image, ImageAdmin)
+
+#class TeamAdmin(admin.ModelAdmin):
+#    list_display = ('name','photo')
+#    search_fields = ('name',)
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name','thumb')
+    search_fields = ('name','nickname','rut')
+    list_filter = ('team',)
+    list_filter = ('birthdate',)
+    def thumb(self,obj):
+        return mark_safe("<img src='%s' width='40' height='40'>"% obj.photo.url)
+    thumb.allow_tags = True
+    thumb.__name__ = 'photo'
+
+@admin.register(Coach)
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ('name','age')
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name','code')
--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,60 @@
+import os
+#from __future__ import unicode_literals
 from django.db import models
+from django.conf import settings
+from django.utils.safestring import mark_safe
 
-# Create your models here.
+
+class Team(models.Model): #herencia de models
+    name = models.CharField(max_length=200) #maximo del string
+    description = models.TextField()
+    logo = models.ImageField()
+    # photo = models.ImageField(upload_to='photo')
+    code = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=200)
+    birthdate = models.DateField()
+    age = models.IntegerField(default = 0)
+    rut = models.CharField(max_length=200)
+    mail = models.EmailField(max_length=200)
+    height = models.FloatField(max_length=200)
+    weight = models.IntegerField(default = 0)
+    photo = models.ImageField()
+    #photo = models.ImageField(upload_to='photo')
+    team = models.ForeignKey(Team, on_delete = models.CASCADE)
+    position = models.CharField(
+            max_length=2,
+            choices=(
+                    ('BA', 'Base'),
+                    ('ES', 'Escolta'),
+                    ('AL', 'Alero'),
+                    ('AP', 'AlaPivot'),
+                    ('PI', 'Pivot'),
+                    ),
+            default='BA'
+            )
+
+    def __str__(self):
+        return self.name
+
+class Coach(models.Model):
+    name = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=200)
+    age = models.IntegerField(default = 0)
+    rut = models.CharField(max_length=200)
+    mail = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
